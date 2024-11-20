129c61e192b718cd09903e0d59ef6d405f9fda20
ena-isme <tlswldnjs410@gmail.com>
2024-11-20 17:30:56 +0900

    #2 - 컬러 팔레트 구현

--- src/views/Sketch.js
+++ src/views/Sketch.js
@@ -1,13 +1,19 @@
-import {useCallback, useState, useEffect, useRef} from 'react';
+import { useCallback, useState, useEffect, useRef } from 'react';
 import Button from '@enact/sandstone/Button';
-import {fabric} from 'fabric';
+import { fabric } from 'fabric';
+import { CirclePicker } from 'react-color';
 
 const Sketch = () => {
 	const [canvas, setCanvas] = useState(null);
 	const bgColor = useRef('#FFFFFF');
 
 	// 그리기/선택 모드 구분
-	const [activeTool, setActiveTool] = useState("select");
+	const [activeTool, setActiveTool] = useState("draw");
+
+	// 브러쉬 초기 설정
+	const [lineWidth, setLineWidth] = useState(2);
+	const [brushColor, setBrushColor] = useState("#000000");
+	const [showPicker, setShowPicker] = useState(false);
 
 	useEffect(() => {
 		const initCanvas =
@@ -15,7 +21,11 @@ const Sketch = () => {
 				height: 780,
 				width: 1700,
 				backgroundColor: bgColor.current,
-		});
+				isDrawingMode: true,
+			});
+
+		initCanvas.freeDrawingBrush.color = brushColor;
+		initCanvas.freeDrawingBrush.width = lineWidth;
 		setCanvas(initCanvas);
 
 		return () => {
@@ -23,7 +33,6 @@ const Sketch = () => {
 		};
 	}, []);
 
-	// 그리기/선택 모드 구분
 	useEffect(() => {
 		if (canvas) {
 			if (activeTool == "draw") {
@@ -42,20 +51,57 @@ const Sketch = () => {
 		canvas.backgroundColor = bgColor.current;
 	}, [canvas]);
 
-	const downloadCanvas = useCallback(() => {}, []);
+	const downloadCanvas = useCallback(() => { }, []);
 
+	//MARK: - 그리기/선택 모드 구분
 	const setDrawMode = useCallback(() => {
 		canvas.isDrawingMode = true;
-				canvas.selection = false;
-				canvas.renderAll();
-				canvas.forEachObject((obj) => (obj.selectable = false));
+		canvas.selection = false;
+		canvas.renderAll();
+		canvas.forEachObject((obj) => (obj.selectable = false));
 	}, [canvas]);
 
 	const setSelectMode = useCallback(() => {
 		canvas.isDrawingMode = false;
-				canvas.selection = true;
-				canvas.forEachObject((obj) => (obj.selectable = true));
-				canvas.renderAll();
+		canvas.selection = true;
+		canvas.forEachObject((obj) => (obj.selectable = true));
+		canvas.renderAll();
+	}, [canvas]);
+
+
+	//MARK: - Line 두께 설정
+	const changeLightWeight = useCallback(() => {
+		if (canvas) {
+			const newWidth = 2;
+			canvas.freeDrawingBrush.width = newWidth;
+		}
+	}, [canvas]);
+
+	const changeMediumWeight = useCallback(() => {
+		if (canvas) {
+			const newWidth = 6;
+			canvas.freeDrawingBrush.width = newWidth;
+		}
+	}, [canvas]);
+
+	const changeBoldWeight = useCallback(() => {
+		if (canvas) {
+			const newWidth = 10;
+			canvas.freeDrawingBrush.width = newWidth;
+		}
+	}, [canvas]);
+
+	//MARK: - Line 색상 설정
+	// const showColorPallete = useCallback(() => {
+	// 	setShowPicker((prev) => !prev);
+	// }, []);
+
+	const changeBrushColor = useCallback((color) => {
+		const selectedColor = color.hex;
+		setBrushColor(selectedColor);
+		if (canvas) {
+			canvas.freeDrawingBrush.color = selectedColor;
+		}
 	}, [canvas]);
 
 	return (
@@ -65,12 +111,6 @@ const Sketch = () => {
 				iconOnly
 				backgroundOpacity="opaque"
 				onClick={clearCanvas}
-			/>
-			<Button
-				icon="download"
-				iconOnly
-				backgroundOpacity="opaque"
-				onClick={downloadCanvas}
 			/>
 			<Button
 				icon="P"
@@ -84,6 +124,38 @@ const Sketch = () => {
 				backgroundOpacity="opaque"
 				onClick={setSelectMode}
 			/>
+			<Button
+				icon="L"
+				iconOnly
+				backgroundOpacity="opaque"
+				onClick={changeLightWeight}
+			/>
+			<Button
+				icon="M"
+				iconOnly
+				backgroundOpacity="opaque"
+				onClick={changeMediumWeight}
+			/>
+			<Button
+				icon="B"
+				iconOnly
+				backgroundOpacity="opaque"
+				onClick={changeBoldWeight}
+			/>
+			<div style={{
+				position: "fixed",
+				top: "20px",
+				left: "1400px",
+				backgroundColor: "white",
+				zIndex: 10
+			}}>
+				<CirclePicker
+					width="700px"
+					height="32px"
+					color={brushColor}
+					onChangeComplete={changeBrushColor}
+				/>
+			</div>
 			<canvas id="canvas" />
 		</div>
 	);
