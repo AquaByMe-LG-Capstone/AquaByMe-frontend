adef6600b054df86a858c470184bbee5a188363f
its-gayeon <gayeonl0007@gmail.com>
2024-12-11 21:48:50 +0900

    #19 - Fishes are swimming

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,136 +1,43 @@
 /* eslint-disable */
-import React, { useRef, useState } from "react";
-import { Shaders, Node, GLSL } from "gl-react";
-import { Surface } from "gl-react-dom";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import Button from '@enact/sandstone/Button';
 import styles from "./Main.module.less";
-
-const shaders = Shaders.create({
-	aquarium: {
-		frag: GLSL`
-		precision highp float;
-		varying vec2 uv;
-		// uniform float u_daytime;
-		uniform float u_time;
-
-		// Color of bottom water
-		const vec3 bottomCol = vec3(0.0588, 0.3098, 0.5647);
-
-		// Colors for different times of the day
-		const vec3 morningCol = vec3(0.8078, 0.8863, 1.0);  // Soft blue (morning)
-		const vec3 noonCol = vec3(0.6157, 0.8078, 1.0);     // Bright blue (noon)
-		const vec3 eveningCol = vec3(1.0, 0.6745, 0.5137);  // Orange-pink (evening)
-		const vec3 nightCol = vec3(0.0863, 0.0863, 0.2667);    // Dark blue (night)
-
-		// number of waves
-		const int waveCnt = 4;
-
-		// Returns sky color based on time
-		vec3 getSkyCol() {
-			float dayTime = mod(u_time / 60.0, 1.0);
-			// Smooth blend for each phase of the day
-			float morningToNoon = smoothstep(0.0, 0.25, dayTime) * (1.0 - smoothstep(0.25, 0.5, dayTime));
-			float noonToEvening = smoothstep(0.25, 0.5, dayTime) * (1.0 - smoothstep(0.5, 0.75, dayTime));
-			float eveningToNight = smoothstep(0.5, 0.75, dayTime) * (1.0 - smoothstep(0.75, 1.0, dayTime));
-			float nightToMorning = smoothstep(0.75, 1.0, dayTime) * (1.0 - smoothstep(0.0, 0.25, dayTime));
-
-			// Combine weights and interpolate colors
-			vec3 dayColor = morningCol * morningToNoon +
-				noonCol * noonToEvening +
-				eveningCol * eveningToNight +
-				nightCol * nightToMorning;
-
-			return dayColor;
-		}
-		
-		float waveComponent(vec2 uv, vec3 look) {
-			float waveFreq = 5.0;
-			vec2 p = uv * waveFreq;
-			float t = u_time * 3.0; // Time variable for wave animation
-
-			// Wave contributions
-			float bump = sin(p.x * 2.0 + t + sin(p.y * 0.73 + t));
-			bump += sin(p.x * 1.43 + t) * 0.5;
-
-			float u = dot(look, vec3(0., 1., 0.));
-			bump *= u * smoothstep(9., 1., u);
-			bump *= smoothstep(0.5, 1., u) * 0.5;
-
-			return bump;
-		}
-
-		// Returns water color based on depth
-		vec3 waterCol() {
-			float depth = uv.y;
-			vec3 color = mix(bottomCol, getSkyCol(), smoothstep(0.3 * depth, 1., depth));
-
-			// calculate and add waves at the top
-			vec2 uv2 = uv * 2.0 - 1.0;
-			uv2.y *= uv.y / uv.x; // Correct for aspect ratio
-
-			// Compute wave height at the current position
-			vec3 lookVec = vec3(0., 0.634, 0.);
-			float waveHeight = 0.;
-
-			for(int i = 0; i < waveCnt; i++) {
-				float offsetFactor = float(i);
-				float wave = waveComponent(uv2 + vec2(0.1 * offsetFactor, 1. / (offsetFactor + 2.)), lookVec) - 0.16 * offsetFactor;
-				waveHeight += smoothstep(0.016, 0., abs(wave - uv2.y + 1.2 + 0.2 * offsetFactor));
-			}
-
-			// color += 0.3 * waveHeight * vec3(0.1, 0.2, 0.4);
-			return color;
-		}
-
-		void main() {
-			gl_FragColor = vec4(waterCol(), 1.0);
-		}`
-	}
-});
-
-// function getPortionedTime() {
-// 	const today = new Date();
-// 	let hour = today.getHours() / 23;
-// 	return hour;
-// }
-
-class Aquarium extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			u_time: 0,
-		};
-		this.startT = null;
-		this.loop = this.loop.bind(this);
-	}
-
-	componentDidMount() {
-		this.startT = performance.now();
-		requestAnimationFrame(this.loop); // eslint-disable-line no-undef
-	}
-
-	componentWillUnmount() {
-		cancelAnimationFrame(this.rafId); // eslint-disable-line no-undef
-	}
-
-	loop(t) {
-		const elapsedTime = (t - this.startT) / 1000; // Elapsed time in seconds
-		this.setState({ u_time: elapsedTime });
-		this.rafId = requestAnimationFrame(this.loop); // eslint-disable-line no-undef
-	}
-
-	render() {
-		const { u_time } = this.state;
-		// const { u_time_of_day } = getPortionedTime();
-		return <Node shader={shaders.aquarium} uniforms={{ u_time }} />;
-	}
-}
+import CONFIG from "../config";
+import axios from "axios";
+import { fabric } from 'fabric';
 
 const Home = () => {
+	// Load the stickers from the server
+	const authToken = window.localStorage.getItem('authToken');
+	const [shownStickers, setShownStickers] = useState([]);
+
+	const loadDrawing = useCallback(() => {
+		const url = `http://${CONFIG.ipAddress}:${CONFIG.port}/gallery/`;
+
+		axios.get(url, {
+			headers: {
+				'Authorization': `Token ${authToken}`,
+			},
+		})
+			.then((resp) => {
+				const allStickers = resp.data;
+				const shown = allStickers.filter((sticker) => sticker.shows === true);
+
+				setShownStickers(shown);
+			})
+			.catch((error) => {
+				console.error('Error loading drawing:', error.message);
+			});
+	}, [authToken]);
+
+	React.useEffect(() => {
+		loadDrawing();
+	}, [loadDrawing]);
+
+	// Fullscreen-related logic
 	const containerRef = useRef(null);
 	const [isFullScreen, setIsFullScreen] = useState(false);
 
-	// 전체화면으로 전환하는 함수
 	const enterFullscreen = () => {
 		if (containerRef.current) {
 			if (containerRef.current.requestFullscreen) {
@@ -145,7 +52,6 @@ const Home = () => {
 		}
 	};
 
-	// 전체화면을 종료하는 함수
 	const exitFullscreen = () => {
 		if (document.exitFullscreen) {
 			document.exitFullscreen();
@@ -158,7 +64,6 @@ const Home = () => {
 		}
 	};
 
-	// 전체 화면 토글 & isFullScreen state 업데이트
 	const toggleFullscreen = () => {
 		if (document.fullscreenElement) {
 			setIsFullScreen(false);
@@ -169,23 +74,108 @@ const Home = () => {
 		}
 	};
 
+	// Actual aquarium (Fabric.js canvas)
+	const aquariumRef = useRef(null);
+	const fishArrayRef = useRef([]);
+
+	useEffect(() => {
+		// Initialize the Fabric.js canvas
+		const canvas = new fabric.Canvas(aquariumRef.current);
+
+		const setCanvasSize = () => {
+			const width = isFullScreen ? window.innerWidth : window.innerWidth * 0.8;
+			const height = isFullScreen ? window.innerHeight : window.innerHeight * 0.87;
+
+			aquariumRef.current.width = width;
+			aquariumRef.current.height = height;
+			canvas.setWidth(width);
+			canvas.setHeight(height);
+			canvas.renderAll();
+		};
+
+		setCanvasSize();
+
+		// Create the background gradient
+		const waterColor = new fabric.Gradient({
+			type: 'linear',
+			coords: { x1: 0, y1: 0, x2: 0, y2: canvas.height },
+			colorStops: [
+				{ offset: 0, color: 'white' },
+				{ offset: 0.5, color: '#BBEBFF' },
+				{ offset: 1, color: '#76D6FF' },
+			],
+		});
+
+		canvas.setBackgroundColor(waterColor, canvas.renderAll.bind(canvas));
+
+		// Function to create and animate a fish using SVG
+		const createFish = (svgData) => {
+			const isLeft = Math.random() > 0.5; // 50% chance to start from left or right
+
+			fabric.loadSVGFromString(svgData, (objects, options) => {
+				const fish = fabric.util.groupSVGElements(objects, options);
+				fish.set({
+					left: isLeft ? -50 : canvas.width + 50, // Position outside canvas initially
+					top: Math.random() * canvas.height, // Random vertical position
+					originX: 'center',
+					originY: 'center',
+					selectable: false,
+				});
+
+				fish.scale(0.3);
+
+				// Add the fish to canvas
+				canvas.add(fish);
+				fishArrayRef.current.push(fish); // Store reference for the fish
+				canvas.renderAll();
+			});
+		};
+
+		// Function to animate the fish
+		const animateFish = () => {
+			fishArrayRef.current.forEach((fish) => {
+				const flip = Math.random() > 0.5;
+				const newX = Math.random() * (canvas.width / 8) + fish.left;
+				const newY = Math.random() * (canvas.height / 5) + fish.top;
+
+				fish.animate({ left: newX, top: newY }, {
+					duration: Math.random() * (4000 - 1000) + 1000, // Animation duration
+					onChange: canvas.renderAll.bind(canvas),
+					easing: fabric.util.ease.easeOutSine, // Smooth easing
+					onComplete: () => animateFish(), // Repeat the animation once complete
+				});
+			});
+		};
+
+		// Once stickers are loaded, create the fish using SVG data
+		let len = shownStickers.length
+		if (len > 0) {
+			for (let i = 0; i < len; i++) {
+				const svgData = shownStickers[i].sticker.svg; // Select a random sticker's SVG data
+				createFish(svgData);
+			}
+
+			// Start animating the fish
+			animateFish();
+		}
+
+	}, [isFullScreen, shownStickers]); // Re-run when stickers or fullscreen state change
+
 	return (
 		<>
 			<div ref={containerRef}>
-				<Surface width={isFullScreen ? '100vw' : '80vw'} height={isFullScreen ? '100vh' : '87vh'}>
-					<Aquarium />
-				</Surface>
+				<canvas ref={aquariumRef} />
 				<Button
 					className={styles.fsButton}
 					backgroundOpacity="transparent"
 					size="small"
 					icon="fullscreen"
 					onClick={toggleFullscreen}
-				>
-				</Button>
+				/>
 			</div>
 		</>
 	);
 };
+
 
 export default Home;
